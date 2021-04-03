108170ded6c607af3cf00d78071b0b1c45e48046
Andre <info@pcgames.lt>
2021-04-03 10:03:33 +0300

    закончено модальное окно login

--- build/js/burger.js
+++ build/js/burger.js
@@ -1,28 +1,34 @@
 'use strict';
 
-var burger = document.querySelector('.burger');
-var headerNav = document.querySelector('.header__nav');
-var logo = document.querySelector('.header__logo');
-var cart = document.querySelector('.header__cart');
-var headerTop = document.querySelector('.header__top');
+(function () {
+  var burger = document.querySelector('.burger');
+  var headerNav = document.querySelector('.header__nav');
+  var logo = document.querySelector('.header__logo');
+  var cart = document.querySelector('.header__cart');
+  var headerTop = document.querySelector('.header__top');
 
-if (burger && headerNav) {
-  burger.addEventListener('click', function () {
-    headerNav.classList.toggle('header__nav--hidden');
-    burger.classList.toggle('burger--white');
+  if (burger && headerNav) {
+    // headerNav.classList.add('header__nav--hidden');
+    // headerNav.classList.add('header__nav--fullscreen');
 
-    if (logo) {
-      logo.classList.toggle('header__logo--white');
-    }
+    burger.addEventListener('click', function () {
+      headerNav.classList.toggle('header__nav--hidden');
+      burger.classList.toggle('burger--white');
 
-    if (cart) {
-      cart.classList.toggle('header__cart--white');
-    }
+      if (logo) {
+        logo.classList.toggle('header__logo--white');
+      }
 
-    if (headerTop) {
-      headerTop.classList.toggle('header__top--brown');
-    }
+      if (cart) {
+        cart.classList.toggle('header__cart--white');
+      }
 
-  });
-}
+      if (headerTop) {
+        headerTop.classList.toggle('header__top--brown');
+      }
+
+    });
+  }
+})();
+
 
---
+++ source/js/modal.js
@@ -0,0 +1,138 @@
+'use strict';
+
+(function () {
+  var openWindowButtons = document.querySelectorAll('[data-modal-id]');
+  // var modals = document.querySelectorAll('.modal');
+  var body = document.querySelector('body');
+  var html = document.querySelector('html');
+  // var scrollPosition = 0;
+  var onModalPressTab;
+  var onModalPressEsc;
+
+  function showModal(modal) {
+    modal.classList.add('modal--active');
+    lockBodyOnModalOpen();
+
+    closeFocusOnModal(modal);
+
+    closeWindowByClick(modal);
+  }
+
+  function hideModal(modal) {
+    modal.classList.remove('modal--active');
+    unlockBodyOnModalClose();
+    document.removeEventListener('keydown', onModalPressTab);
+    document.removeEventListener('keydown', onModalPressEsc);
+  }
+
+  // Закрывает активное окно при клике на кнопки с классом js-close-window и ESC
+  function closeWindowByClick(modal) {
+    if (modal) {
+      var onModalClick = function (evt) {
+        var element = evt.target;
+
+        if (element.classList.contains('js-close-window')) {
+          hideModal(modal);
+          modal.removeEventListener('click', onModalClick);
+        }
+      };
+
+      onModalPressEsc = function (evt) {
+        if (evt.key === 'Escape') {
+          evt.preventDefault();
+          hideModal(modal);
+        }
+      };
+
+      modal.addEventListener('click', onModalClick);
+      document.addEventListener('keydown', onModalPressEsc);
+    }
+  }
+
+
+  // Запераем фокус в модальном окне
+  function closeFocusOnModal(modal) {
+    var focusSelectors = [
+      'a[href]',
+      'area[href]',
+      'input:not([disabled]):not([type="hidden"]):not([aria-hidden])',
+      'select:not([disabled]):not([aria-hidden])',
+      'textarea:not([disabled]):not([aria-hidden])',
+      'button:not([disabled]):not([aria-hidden])',
+      'iframe',
+      'object',
+      'embed',
+      '[contenteditable]',
+      '[tabindex]:not([tabindex^="-"])'
+    ];
+
+    var focusElements = modal.querySelectorAll(focusSelectors);
+    // console.log(focusElements[0]);
+    if (focusElements) {
+      var focusedIndex = 0;
+      var lastIndex = focusElements.length - 1;
+      focusElements[0].focus();
+
+      onModalPressTab = function (evt) {
+        if (evt.keyCode === 9) {
+          evt.preventDefault();
+          // Shift + TAB
+          if (evt.shiftKey) {
+            if (focusedIndex > 0) {
+              focusedIndex--;
+            } else {
+              focusedIndex = lastIndex;
+            }
+          } else { // TAB
+            if (focusedIndex < lastIndex) {
+              focusedIndex++;
+            } else {
+              focusedIndex = 0;
+            }
+          }
+
+          focusElements[focusedIndex].focus();
+        }
+      };
+
+      document.addEventListener('keydown', onModalPressTab);
+    }
+  } // End Of Function
+
+
+  // Фиксируем страницу при открытии окна
+  var lockBodyOnModalOpen = function () {
+    var marginSize = window.innerWidth - html.clientWidth;
+    body.classList.add('modal-opened');
+
+    // ширина скроллбара равна разнице ширины окна и ширины документа (селектора html)
+    if (marginSize) {
+      html.style.marginRight = marginSize + 'px';
+    }
+  };
+
+  // Снимаем фиксацию страницы при закрыкрытии окна
+  var unlockBodyOnModalClose = function () {
+    body.classList.remove('modal-opened');
+    html.style.marginRight = '';
+  };
+
+
+  // При клике на любую кнопку с атрибутом data-modal-id -
+  //  открываем окно с id указанным в data атрибуте кнопки
+  openWindowButtons.forEach(function (elem) {
+    elem.addEventListener('click', function () {
+      var modalId = '#' + elem.dataset.modalId;
+      var modal = document.querySelector(modalId);
+
+
+      if (modal) {
+        showModal(modal);
+      }
+    });
+  });
+
+
+})();
+
+
